6a9e0eba80a532ecbb8eb66597c74fc00e05b2d2
raphminkyu <leer@dickinson.edu>
2021-09-23 20:30:26 -0400

    a

--- Calculator.java
+++ Calculator.java
@@ -85,7 +85,7 @@ public class Calculator {
 	 * @return the area of a rectangle with sides x and y.
 	 */
 	public double rectangleArea(double x, double y) {
-		return 2 * x + 2 * y;
+		return 2 * x + 2 * y; 
 	}
 
 	/**
